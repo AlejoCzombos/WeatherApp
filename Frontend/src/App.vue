1f3f3d472b9afb638a8c0749cfe847281668e616
Alejo <alejoczombos@gmail.com>
2024-03-27 10:47:46 -0300

    Commit inicial

---
+++ Frontend/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <RouterView />
+</template>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Quicksand:wght@300..700&display=swap');
+#app {
+  font-family: 'Quicksand', sans-serif;
+}
+</style>
---
+++ Frontend/src/views/Home.vue
@@ -0,0 +1,12 @@
+<script>
+export default {
+  name: 'Home',
+  components: {}
+}
+</script>
+
+<template>
+  <main>
+    <h1 class="hola">Hola mundo</h1>
+  </main>
+</template>
