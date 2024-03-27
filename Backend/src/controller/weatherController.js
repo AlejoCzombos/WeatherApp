08ef37194352fb816411acd70a51aba2d46fd460
Alejo <alejoczombos@gmail.com>
2024-03-27 19:37:10 -0300

    Controller

---
+++ Backend/src/controller/weatherController.js
@@ -0,0 +1,17 @@
+const weatherService = require('../service/weatherService');
+
+exports.getWeather = async (req, res) => {
+    return await weatherService.getWeather(req, res);
+}
+
+exports.addCity = async (req, res) => {
+    return await weatherService.addCity(req, res);
+}
+
+exports.getCities = async (req, res) => {
+    return await weatherService.getCities(res);
+}
+
+exports.deleteCity = async (req, res) => {
+    return await weatherService.deleteCity(req, res);
+}
