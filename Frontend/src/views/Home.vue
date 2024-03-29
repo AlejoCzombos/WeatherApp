24af3e349b767dda65e5f74402a99b2698af794e
Alejo <alejoczombos@gmail.com>
2024-03-29 12:41:12 -0300

    grid y prueba con los cards

--- Frontend/src/views/Home.vue
+++ Frontend/src/views/Home.vue
@@ -1,12 +1,20 @@
 <script>
+import CityCard from '@/components/CityCard.vue'
 export default {
   name: 'Home',
-  components: {}
+  components: { CityCard }
 }
 </script>
 
 <template>
-  <main>
-    <h1 class="hola">Hola mundo</h1>
+  <main class="max-w-4xl m-auto grid sm:grid-cols-2 pt-20 bg-slate-900 min-h-screen min-h">
+    <CityCard />
+    <CityCard />
+    <CityCard />
+    <CityCard />
+    <CityCard />
+    <CityCard />
+    <CityCard />
+    <CityCard />
   </main>
 </template>
