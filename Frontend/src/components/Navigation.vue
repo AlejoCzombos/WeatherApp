ea84c0cecf4a254d364be54a32c7e36f95a89141
Alejo <alejoczombos@gmail.com>
2024-03-29 12:40:14 -0300

    Navigation Component

---
+++ Frontend/src/components/Navigation.vue
@@ -0,0 +1,29 @@
+<script>
+import ModifyIcon from '@/components/icons/ModifyIcon.vue'
+import RefreshIcon from '@/components/icons/RefreshIcon.vue'
+import PlusIcon from '@/components/icons/PlusIcon.vue'
+
+export default {
+  name: 'Navigation',
+  components: {
+    ModifyIcon,
+    RefreshIcon,
+    PlusIcon
+  }
+}
+</script>
+
+<template>
+  <header class="fixed top-0 right-0 w-full bg-slate-800 py-5 z-50 drop-shadow-md">
+    <nav
+      class="flex flex-row justify-between items-center max-w-4xl mx-auto px-5 text-white font-semibold"
+    >
+      <span>Ciudades</span>
+      <ul class="flex flex-row gap-4">
+        <ModifyIcon class="size-9 p-1 hover:scale-125 transition-all ease-in-out" />
+        <RefreshIcon class="size-9 p-1 hover:scale-125 transition-all ease-in-out" />
+        <PlusIcon class="size-9 p-1 hover:scale-125 transition-all ease-in-out" />
+      </ul>
+    </nav>
+  </header>
+</template>
