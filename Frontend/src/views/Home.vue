2abaee58382a10583af981833d36baeb77076e97
Alejo <alejoczombos@gmail.com>
2024-03-30 18:35:51 -0300

    fetch a la api y pasar por parametros las ciudades e información

--- Frontend/src/views/Home.vue
+++ Frontend/src/views/Home.vue
@@ -1,20 +1,25 @@
 <script>
 import CityCard from '@/components/CityCard.vue'
+import { getCitiesWeather } from '@/api/weather.api'
 export default {
   name: 'Home',
-  components: { CityCard }
+  components: { CityCard },
+  data() {
+    return {
+      citiesWeather: []
+    }
+  },
+  async created() {
+    const citiesWeather = await getCitiesWeather()
+    this.citiesWeather = citiesWeather
+  }
 }
 </script>
 
 <template>
   <main class="max-w-4xl m-auto grid sm:grid-cols-2 pt-20 bg-slate-900 min-h-screen min-h">
-    <CityCard />
-    <CityCard />
-    <CityCard />
-    <CityCard />
-    <CityCard />
-    <CityCard />
-    <CityCard />
-    <CityCard />
+    <div v-for="city in citiesWeather" :key="city.city">
+      <CityCard v-bind:city="city" />
+    </div>
   </main>
 </template>
