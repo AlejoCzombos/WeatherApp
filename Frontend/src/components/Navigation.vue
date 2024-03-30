e6f252dd5adcf18bcb4af554af04f8cc7f113531
Alejo <alejoczombos@gmail.com>
2024-03-30 18:34:47 -0300

    cambio de componentes vue a svg y evento para abrir la ventana modal

--- Frontend/src/components/Navigation.vue
+++ Frontend/src/components/Navigation.vue
@@ -1,28 +1,38 @@
 <script>
-import ModifyIcon from '@/components/icons/ModifyIcon.vue'
-import RefreshIcon from '@/components/icons/RefreshIcon.vue'
-import PlusIcon from '@/components/icons/PlusIcon.vue'
-
 export default {
   name: 'Navigation',
-  components: {
-    ModifyIcon,
-    RefreshIcon,
-    PlusIcon
+  components: {},
+  methods: {
+    openModal() {
+      this.$emit('open-modal')
+    }
   }
 }
 </script>
 
 <template>
-  <header class="fixed top-0 right-0 w-full bg-slate-800 py-5 z-50 drop-shadow-md">
+  <header class="fixed top-0 right-0 w-full min-h-20 bg-slate-800 py-5 z-40 drop-shadow-md">
     <nav
       class="flex flex-row justify-between items-center max-w-4xl mx-auto px-5 text-white font-semibold"
     >
       <span>Ciudades</span>
       <ul class="flex flex-row gap-4">
-        <ModifyIcon class="size-9 p-1 hover:scale-125 transition-all ease-in-out" />
-        <RefreshIcon class="size-9 p-1 hover:scale-125 transition-all ease-in-out" />
-        <PlusIcon class="size-9 p-1 hover:scale-125 transition-all ease-in-out" />
+        <img
+          src="/icons/ModifyIcon.svg"
+          alt="modify icon"
+          class="size-9 p-1 hover:scale-125 transition-all ease-in-out"
+        />
+        <img
+          src="/icons/RefreshIcon.svg"
+          alt="refresh icon"
+          class="size-9 p-1 hover:scale-125 transition-all ease-in-out"
+        />
+        <img
+          src="/icons/PlusIcon.svg"
+          alt="Add city icon"
+          @click="openModal"
+          class="size-9 p-1 hover:scale-125 transition-all ease-in-out"
+        />
       </ul>
     </nav>
   </header>
