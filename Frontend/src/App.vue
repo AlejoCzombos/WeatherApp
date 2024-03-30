30f659d21c23ea9c9c52d270aa7f81053c0241fd
Alejo <alejoczombos@gmail.com>
2024-03-30 18:36:10 -0300

    ventana modal y funcionamiento a traves de señales

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -1,18 +1,37 @@
 <script>
 import { RouterView } from 'vue-router'
 import Navigation from './components/Navigation.vue'
+import AddCityModal from '@/components/AddCityModal.vue'
+import { addCity } from '@/api/weather.api'
 
 export default {
   name: 'App',
+  data() {
+    return {
+      modalIsOpen: false
+    }
+  },
   components: {
-    Navigation
+    Navigation,
+    AddCityModal
+  },
+  methods: {
+    onToggleModal() {
+      this.modalIsOpen = !this.modalIsOpen
+    },
+    async addCity(city) {
+      const res = await addCity(city)
+      console.log(res)
+      this.modalIsOpen = false
+    }
   }
 }
 </script>
 
 <template>
   <div class="m-auto bg-slate-900">
-    <Navigation />
+    <AddCityModal v-if="modalIsOpen" v-on:close-modal="onToggleModal" v-on:add-city="addCity" />
+    <Navigation v-on:open-modal="onToggleModal" />
     <RouterView />
   </div>
 </template>
