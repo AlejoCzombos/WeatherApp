30d3e3e71769d2614ff0303197fb732adcba718a
Alejo <alejoczombos@gmail.com>
2024-03-30 18:33:01 -0300

    ventana modal para agregar ciudades

---
+++ Frontend/src/components/AddCityModal.vue
@@ -0,0 +1,50 @@
+<script>
+export default {
+  name: 'AddCityModal',
+  data() {
+    return {
+      city: ''
+    }
+  },
+  methods: {
+    closeModal(e) {
+      if (e.target === this.$refs.modal) {
+        this.$emit('close-modal')
+      }
+    },
+    addCity() {
+      this.$emit('add-city', this.city)
+      this.city = ''
+    }
+  }
+}
+</script>
+
+<template>
+  <div
+    @click="closeModal"
+    ref="modal"
+    class="bg-black/65 fixed top-0 left-0 w-full h-full z-50 flex justify-center items-center"
+  >
+    <section class="bg-slate-800 rounded-lg w-[80%] max-w-xl p-5 shadow-lg text-white">
+      <div class="flex flex-col gap-1">
+        <label class="font-semibold text-lg" for="cityName">Nombre de la ciudad:</label>
+        <input
+          type="text"
+          id="cityName"
+          placeholder="Roma"
+          v-model="city"
+          class="border-b-2 border-slate-500 bg-transparent text-white px-1 py-1.5 focus:outline-none focus:border-slate-300"
+        />
+      </div>
+      <div class="flex flex-row justify-end gap-4 mt-5">
+        <button
+          @click="addCity"
+          class="bg-slate-700 text-white px-4 py-2 rounded-lg hover:bg-slate-600 transition-all ease-in-out"
+        >
+          Agregar
+        </button>
+      </div>
+    </section>
+  </div>
+</template>
