6cb9df38cfcbabe36ee3cafdcd99c2c6906eab50
Alejo <alejoczombos@gmail.com>
2024-04-02 11:20:10 -0300

    llamadas a endpoint de refrescar datos y eliminar ciudad

--- Frontend/src/api/weather.api.js
+++ Frontend/src/api/weather.api.js
@@ -5,9 +5,26 @@ export const getCitiesWeather = async () => {
     return data;
 }
 
+export const refreshWeatherData = async () => {
+    const response = await fetch('http://localhost:8000/api/weather/refresh');
+    const data = await response.text();
+    return data;
+}
+
 export const addCity = async (cityName) => {
     const response = await fetch('http://localhost:8000/api/weather/cities', {
         method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ city: cityName })
+    });
+    return response;
+}
+
+export const deleteCity = async (cityName) => {
+    const response = await fetch('http://localhost:8000/api/weather/cities', {
+        method: 'DELETE',
         headers: {
             'Content-Type': 'application/json'
         },
