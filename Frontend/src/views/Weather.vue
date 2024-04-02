9913d6404eba9c5afcc8735bfe5824e8deefbbb0
Alejo <alejoczombos@gmail.com>
2024-04-02 11:23:59 -0300

    ruta dinamica para cada ciudad

---
+++ Frontend/src/views/Weather.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  name: 'Weather',
+  components: {},
+  created() {
+    console.log(this.$route.params.city)
+  }
+}
+</script>
+
+<template>
+  <main class="max-w-4xl m-auto pt-20 bg-slate-900">
+    <h1>Weather of: {{ this.$route.params.city }}</h1>
+  </main>
+</template>
