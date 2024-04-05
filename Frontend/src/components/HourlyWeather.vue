c6c2daf5860f6bff7b2f83470b0e290b0f25b629
Alejo <alejoczombos@gmail.com>
2024-04-04 21:36:12 -0300

    pronostico por horas

---
+++ Frontend/src/components/HourlyWeather.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+  name: 'HourlyWeather',
+  components: {},
+  props: {
+    hourlyWeather: {
+      required: true,
+      type: Array
+    }
+  }
+}
+</script>
+
+<template>
+  <section class="flex py-4 flex-row overflow-x-scroll w-full h-44 border-slate-300">
+    <div
+      v-for="hourly in hourlyWeather"
+      :key="hourly.index"
+      class="px-5 flex flex-col justify-center items-center text-white text-lg gap-3"
+    >
+      <p>{{ hourly.time }}</p>
+      <img :src="`/conditions/${hourly.status}.svg`" class="size-12 py-1" alt="weather icon" />
+      <p class="text-xl">{{ hourly.temperature }}&deg;</p>
+    </div>
+  </section>
+</template>
