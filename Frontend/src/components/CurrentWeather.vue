ff869a8b4712d69b6a735d5eaa315d5aed979450
Alejo <alejoczombos@gmail.com>
2024-04-05 11:39:40 -0300

    imagen y refactor

--- Frontend/src/components/CurrentWeather.vue
+++ Frontend/src/components/CurrentWeather.vue
@@ -14,30 +14,32 @@ export default {
 </script>
 
 <template>
-  <section>
-    <div class="flex flex-col justify-between items-start text-white gap-10 p-3">
-      <h2 class="text-2xl">{{ city }}</h2>
-      <div>
-        <p class="text-7xl font-semibold">{{ currentWeather.temperature.current }}&deg;</p>
-        <div class="flex gap-3">
-          <div class="flex justify-center items-center gap-1">
-            <img class="size-6" src="/icons/UpIcon.svg" alt="Max temperature icon" />
-            <p>{{ currentWeather.temperature.max }}&deg;</p>
-          </div>
-          <div class="flex justify-center items-center gap-1">
-            <img class="size-6" src="/icons/DownIcon.svg" alt="Min temperature icon" />
-            <p>{{ currentWeather.temperature.min }}&deg;</p>
-          </div>
+  <section class="flex flex-col justify-between items-start text-white gap-16 py-12 relative">
+    <header>
+      <h2 class="text-3xl font-semibold">{{ city }}</h2>
+    </header>
+    <div>
+      <p class="text-7xl font-semibold">{{ currentWeather.temperature.current }}&deg;</p>
+      <div class="flex gap-3">
+        <div class="flex justify-center items-center gap-1">
+          <img class="size-6" src="/icons/UpIcon.svg" alt="Max temperature icon" />
+          <p>{{ currentWeather.temperature.max }}&deg;</p>
+        </div>
+        <div class="flex justify-center items-center gap-1">
+          <img class="size-6" src="/icons/DownIcon.svg" alt="Min temperature icon" />
+          <p>{{ currentWeather.temperature.min }}&deg;</p>
         </div>
       </div>
-      <footer>
-        <p class="text-xl text-semibold">{{ currentWeather.description }}</p>
-        <p class="text-sm text-slate-300">
-          Viento: {{ Math.round(currentWeather.wind.speed) }} km/h
-        </p>
-        <p class="text-sm text-slate-300">Humedad: {{ currentWeather.extras.humidity }}%</p>
-      </footer>
     </div>
-    <img />
+    <footer>
+      <p class="text-xl text-semibold">{{ currentWeather.description }}</p>
+      <p class="text-sm text-slate-300">Viento: {{ Math.round(currentWeather.wind.speed) }} km/h</p>
+      <p class="text-sm text-slate-300">Humedad: {{ currentWeather.extras.humidity }}%</p>
+    </footer>
+    <img
+      class="absolute top-1/2 -right-52 size-[25rem] transform -translate-y-1/2"
+      src="/moon.png"
+      alt="time status"
+    />
   </section>
 </template>
