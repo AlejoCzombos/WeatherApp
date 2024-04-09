9fead098e5d4981a289f09d2913e654536728bc1
Alejo <alejoczombos@gmail.com>
2024-04-09 11:27:53 -0300

    custom colors

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -2,7 +2,23 @@
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        'sky': {
+          '50': '#eff8ff',
+          '100': '#daeeff',
+          '200': '#bee2ff',
+          '300': '#91d0ff',
+          '400': '#5eb6fc',
+          '500': '#3b97f9',
+          '600': '#2277ee',
+          '700': '#1a61db',
+          '800': '#1c4fb1',
+          '900': '#1c458c',
+          '950': '#142a5f',
+        },
+      }
+    }
   },
   plugins: []
 }
